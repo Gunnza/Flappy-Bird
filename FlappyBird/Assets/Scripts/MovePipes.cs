52bd79208ed6f80462fedcb07cd2de29c93079d7
Christopher McGovern <chrismcg_@hotmail.com>
2019-03-25 01:38:24 +0000

    Updated Code Comments

--- FlappyBird/Assets/Scripts/MovePipes.cs
+++ FlappyBird/Assets/Scripts/MovePipes.cs
@@ -1,29 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Give the illussion of the player moving forward by moving the pipes to the left
 public class MovePipes : MonoBehaviour {
 	
+	//init variable for movement
 	float Movement;
 	
+	//boolean to see if pipes are moving
 	public static bool moving = true;
 
-
-	// Use this for initialization
-	void Start () {
-		
-		
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		
+		//if moving is true set move varibale to moving backwards
 		if (moving == true)
-			Movement = -.1f;
+			Movement = -.1f; 
 		
+		//if moving is false stop the pipes from moving 
 		if (moving == false)
 			Movement = 0;
 		
+		//always be transforming the pipes based on the x y z variables 
 		transform.Translate(Movement,0,0);
 	
 	}
