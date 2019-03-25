6809599131c2738f750eb043d47b1ee03d0a2d56
Christopher McGovern <chrismcg_@hotmail.com>
2019-03-25 01:25:50 +0000

    Commenting code

--- FlappyBird/Assets/Scripts/Jump.cs
+++ FlappyBird/Assets/Scripts/Jump.cs
@@ -1,53 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//Tap the screen to jump
 public class Jump : MonoBehaviour {
-	
-	float maxSpeed = 10f;
-	bool clicked = false;
 
-	// Use this for initialization
-	void Start () {
+//init max speed variable 	
+float maxSpeed = 10f;
 	
-	}
+//init clicked boolen variable	
+bool clicked = false;
+
+// Update is called once per frame
+void Update () {
 	
-	// Update is called once per frame
-	void Update () 
-	{
-						
-	  foreach (Touch touch in Input.touches)
-	{
-			
-	if (clicked == false)
-	{
-		if (touch.phase == TouchPhase.Began)
+	//each time the player touches the screen
+	foreach (Touch touch in Input.touches) {
+		
+		//if not already clicked 
+		if (clicked == false) 
 		{
+			//if the touch begins on the screen
+			if (touch.phase == TouchPhase.Began)
+			{
+				//add force to the y axis of the player object
 				rigidbody.AddForce(0,1000,0);
-				clicked = true; 			
-		}			
-	 }
 				
+				//set clicked to true
+				clicked = true; 			
+			}			
+		 }
+
+		//once finger has left the screen
 		if (touch.phase == TouchPhase.Ended)
 		{
+			//cliked is false so user can then jump again
 			clicked = false;
 		}
-	}
-		
+	}		
 }
 			
 	
-	void FixedUpdate() {
-			
-		
-		if (rigidbody.velocity.magnitude > maxSpeed)	
+void FixedUpdate() {
+	
+	//if the player object goes faster than the max speed variable 
+	if (rigidbody.velocity.magnitude > maxSpeed)	
 		{
+			//set the velocity to the maxspeed 
 			 rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
-		}
-			
-	}
-		
-		
-		
+		}		
+	}						
 }
 	
 
